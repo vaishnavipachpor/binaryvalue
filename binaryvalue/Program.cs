21ef0bd0c842c2e735a29cabb3299da2dc828a3f
vaishnavipachpor <59760241+vaishnavipachpor@users.noreply.github.com>
2023-09-06 16:10:45 +0530

    Add project files.

---
+++ binaryvalue/Program.cs
@@ -0,0 +1,23 @@
+﻿//write a program to print binary value of the given number
+class NumberToBinary
+{
+    static void Main()
+    {
+        Console.Write("Enter an Number to convert an Binary");
+        int Number = int.Parse(Console.ReadLine());
+        int[] arr = new int[16];
+        int i;
+        for (i = 0; Number > 0; i++)
+        { 
+            arr[i] = Number % 2;
+            Number = Number / 2;
+        }
+        Console.Write("Binary value of the given number is:");
+        for (i = i - 1; i >= 0; i--)
+        {
+            Console.Write(arr[i]);
+        }
+        Console.ReadLine();
+
+    }
+}
